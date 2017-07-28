75c8bfa5c07873a68defbfd71d8caa4879ba9753
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-07-27 17:34:47 -0700

    trying to merge

--- cart.js
+++ cart.js
@@ -1,29 +1,4 @@
 'use strict';
-//Create a list of items as purchased
-function Store(name){
-  this.name = name;
-  this.itemsPurchased = itemsPurchased;
-  this.cartItems = document.getElementById('orderCartList');
-}
-
-this.render = function() {
-  this.itemsPurchased();
-}
-
-
-
-
-//create button to delete items from form
-
-
-function eventHandler(e){
-  var order = document.getElementById('cartItems')
-
-var deleteButton = document.getElementById('deleteButton');
-deleteButton.addEventListner(click, "eventHandler");
-
-if (e.addItem) e.addItem()
-var itemsPurchased = document.getElementById('cartItems').allNames;
 
 // collect personal info
 var personalInfo = [];
@@ -61,7 +36,7 @@ for(var i = 0; i < personalInfo.length; i++) {
   ulEl.appendChild(liEl);
 }
 
-// add an item 
+// add an item
 var order = document.createElement('li');
 order.textContent = item + quantity;
 var cartItems = document.getElementById('cart_items');
